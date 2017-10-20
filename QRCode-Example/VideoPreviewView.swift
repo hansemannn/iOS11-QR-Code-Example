082334033eac4600451f543cf6431be755c697ee
Hans Knöchel <hans.knoechel@hs-osnabrueck.de>
2017-10-20 19:58:09 +0200

    Refactor pull-request

--- QRCode-Example/VideoPreviewView.swift
+++ QRCode-Example/VideoPreviewView.swift
@@ -10,35 +10,34 @@ import UIKit
 import AVKit
 
 class VideoPreviewView : UIView {
-    
-    var videoPreviewLayer: AVCaptureVideoPreviewLayer {
-        return layer as! AVCaptureVideoPreviewLayer
+  
+  var videoPreviewLayer: AVCaptureVideoPreviewLayer {
+    return layer as! AVCaptureVideoPreviewLayer
+  }
+  
+  var session: AVCaptureSession? {
+    get { return videoPreviewLayer.session }
+    set { videoPreviewLayer.session = newValue }
+  }
+  
+  override class var layerClass: AnyClass {
+    return AVCaptureVideoPreviewLayer.self
+  }
+  
+  private var orientationMap: [UIDeviceOrientation : AVCaptureVideoOrientation] = [
+    .portrait : .portrait,
+    .portraitUpsideDown : .portraitUpsideDown,
+    .landscapeLeft : .landscapeRight,
+    .landscapeRight : .landscapeLeft,
+  ]
+  
+  func updateVideoOrientationForDeviceOrientation() {
+    if let videoPreviewLayerConnection = videoPreviewLayer.connection {
+      let deviceOrientation = UIDevice.current.orientation
+      guard let newVideoOrientation = orientationMap[deviceOrientation], deviceOrientation.isPortrait || deviceOrientation.isLandscape else {
+        return
+      }
+      videoPreviewLayerConnection.videoOrientation = newVideoOrientation
     }
-    
-    var session: AVCaptureSession? {
-        get { return videoPreviewLayer.session }
-        set { videoPreviewLayer.session = newValue }
-    }
-    
-    override class var layerClass: AnyClass {
-        return AVCaptureVideoPreviewLayer.self
-    }
-    
-    private var orientationMap: [UIDeviceOrientation : AVCaptureVideoOrientation] = [
-        .portrait : .portrait,
-        .portraitUpsideDown : .portraitUpsideDown,
-        .landscapeLeft : .landscapeRight,
-        .landscapeRight : .landscapeLeft,
-    ]
-    
-    func updateVideoOrientationForDeviceOrientation() {
-        if let videoPreviewLayerConnection = videoPreviewLayer.connection {
-            let deviceOrientation = UIDevice.current.orientation
-            guard let newVideoOrientation = orientationMap[deviceOrientation], deviceOrientation.isPortrait || deviceOrientation.isLandscape else {
-                return
-            }
-            videoPreviewLayerConnection.videoOrientation = newVideoOrientation
-        }
-    }
-    
+  }
 }
